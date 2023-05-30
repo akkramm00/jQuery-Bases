1a7e502440a23e4d6ad10cc9568f547af946c6aa
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-30 10:20:35 +0200

    add second script for chaging the list-items with the .data.method()

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ $(() => {
 
  $('#introduction-copy').html($('#introduction').html())
 
+//Initialisez la liste d'éléments #goals grâce aux valeurs suivantes :
+// Récupérer le code HTML
+// Modifier le CSS
+// Parcourir une liste d'éléments
+// Associer des données à un élément
 
+
+    let element = $( "#goals" )
+  
+    $.data( element, "goals", {
+      0 : "Récupérer le code HTML",
+      1 : "Modifier le CSS",
+      2 : "Parcourir une liste d'éléments",
+      3 : "Associer des données à un élément"
+    })
+  
+    $('#goals li').each(function(index) {
+      $(this).text($.data( element, "goals" )[index])
+    })
+  
+  
 
 });
