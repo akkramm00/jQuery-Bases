a5a2739018ee177a7b1c7d1a3a69ced0ff168c81
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-30 10:13:28 +0200

    add script of duplicate the paragraph

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(() => {
     alert('jQuery est bien configuré !')
+
+//Dupliquons le contenu de #introduction pour l'insérer dans #introduction-copy.
+
+ $('#introduction-copy').html($('#introduction').html())
+
+
+
 });
